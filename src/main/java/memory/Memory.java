a37dbbe4885d992ed5ecec9e067b5df9625bb1c3
zidol <ajh3166@naver.com>
2023-05-29 15:52:19 +0900

    자동구선 직접 만드는 예제

---
+++ src/main/java/memory/Memory.java
@@ -0,0 +1,29 @@
+package memory;
+
+public class Memory {
+
+    private long used;
+
+    private long max;
+
+    public Memory(long used, long max) {
+        this.used = used;
+        this.max = max;
+    }
+
+    public long getMax() {
+        return max;
+    }
+
+    public long getUsed() {
+        return used;
+    }
+
+    @Override
+    public String toString() {
+        return "Memory{" +
+                "used=" + used +
+                ", max=" + max +
+                '}';
+    }
+}
